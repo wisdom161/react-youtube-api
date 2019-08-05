d95b05d86486fc1bbf02d2fb247eace9e86b3170
Wisdom Liu <wisdom.code161@gmail.com>
2019-08-05 14:03:29 -0700

    created video details

--- src/App.js
+++ src/App.js
@@ -7,27 +7,43 @@ import { SearchBar, VideoDetails } from './components'
 import youtube from './api/youtube';
 
 class App extends Component {
+  state = {
+    videos: [],
+    selectedVideo: null,
+  }
+
   handleSubmit = async (searchTerm) => {
     const response = await youtube.get('search', { 
       params: {
         part: 'snippet',
         maxResults: 5,
-        key: 'AIzaSyD3Zk3CtIMj4B5kkVdtBhDf4O9i_lFovjU'
+        key: 'AIzaSyD3Zk3CtIMj4B5kkVdtBhDf4O9i_lFovjU',
+        q: searchTerm,
       }
     });
 
-    console.log(response);
+    console.log('this is response', response.data.items);
+
+    this.setState( 
+      { 
+        videos: response.data.items, 
+        selectedVideo: response.data.items[0]
+      }
+    )
+
   }
   render() {
+    const { selectedVideo } = this.state;
+
     return (
       <Grid justify="center" container spacing={10}>
         <Grid item xs={12}>
           <Grid container spacing={10}>
             <Grid item xs={12}>
-              <SearchBar onFormSubmit={this.handleSubmit}/> 
+              <SearchBar onFormSubmit={this.handleSubmit} /> 
             </Grid>
             <Grid item xs={8}>
-              <VideoDetails />
+              <VideoDetails video={selectedVideo} />
             </Grid>
             <Grid item xs={4}>
               {/*video list*/}
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as SearchBar } from './SearchBar';
 export { default as VideoDetails } from './VideoDetails';
+export { default as VideoList } from './VideoList';
